b579050f09e1216559dfd463d92081b7cf0dd3f3
Noah <noah.waldner@valtech.com>
2021-12-22 14:43:39 +0100

    now pic

--- freespace.sh
+++ freespace.sh
@@ -1,4 +1,4 @@
 USERNAME=$(whoami)
 echo $USERNAME
-curl -o /Users/$USERNAME/Pictures/pranked.jpeg https://i.ytimg.com/vi/n6CDazqyCyc/maxresdefault.jpg
+curl -o /Users/$USERNAME/Pictures/pranked.jpeg https://image.winudf.com/v2/image/Y29tLnRhbzEwMS5wcmFua2FwcC5oYWNrcHJhbmtfc2NyZWVuXzBfZGY2bm1pYzU/screen-0.jpg?fakeurl=1&type=.jpg
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "/Users/'${USERNAME}'/Pictures/pranked.jpeg"'
